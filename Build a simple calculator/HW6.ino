afe469a1a02da659e95ff8e17aa01756044159bb
Patricia Figueroa <patricia.figueroa@ucdenver.edu>
2017-09-26 22:55:45 -0600

    Update HW6.ino

--- Build a simple calculator/HW6.ino
+++ Build a simple calculator/HW6.ino
@@ -1,1 +1,129 @@
 
+//Patricia Figueroa 
+// HW # 6: Build a simple calculator 
+// Sept. 25, 2017 
+
+
+#include <LiquidCrystal_I2C.h>                              
+#include <Keypad.h>
+#include <Wire.h>
+
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+long first = 0;
+long second = 0;
+double total = 0;
+
+char customKey;
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1','2','3','+'},
+  {'4','5','6','-'},
+  {'7','8','9','*'},
+  {'C','0','=','/'}
+};
+byte rowPins[ROWS] = {11,10,9, 8}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {7,6,5,4}; //connect to the column pinouts of the keypad
+
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS); 
+
+void setup()
+{
+ lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print("Hello! ");
+  lcd.setCursor(0,1);
+  lcd.print("    Welcome to    ");
+  delay(5000);
+  lcd.setCursor(0,0);
+  lcd.print(" my Arduino  ");
+  lcd.setCursor(0,1);
+  lcd.print("  Calculator !"    );
+  delay(5000);
+  lcd.clear();
+
+  
+}
+void loop()
+{
+  customKey = customKeypad.getKey();
+  switch(customKey) 
+  {
+  case '0' ... '9': // This keeps collecting the first value until a operator is pressed "+-*/"
+    lcd.setCursor(0,0);
+    first = first * 10 + (customKey - '0');
+    lcd.print(first);
+    break;
+
+  case '+':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("+");
+    second = SecondNumber(); // get the second number
+    total = first + second;
+    lcd.setCursor(0,3);
+    lcd.print(total);
+    first = 0, second = 0; // reset values back to zero for next use
+    break;
+
+  case '-':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("-");
+    second = SecondNumber();
+    total = first - second;
+    lcd.setCursor(0,3);
+    lcd.print(total);
+    first = 0, second = 0;
+    break;
+
+  case '*':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("*");
+    second = SecondNumber();
+    total = first * second;
+    lcd.setCursor(0,3);
+    lcd.print(total);
+    first = 0, second = 0;
+    break;
+
+  case '/':
+    first = (total != 0 ? total : first);
+    lcd.setCursor(0,1);
+    lcd.print("/");
+    second = SecondNumber();
+    lcd.setCursor(0,3);
+
+    second == 0 ? lcd.print("Invalid") : total = (float)first / (float)second;
+
+    lcd.print(total);
+    first = 0, second = 0;
+    break;
+
+  case 'C':
+    total = 0;
+    lcd.clear();
+    break;
+  }
+}
+
+long SecondNumber()
+{
+  while( 1 )
+  {
+    customKey = customKeypad.getKey();
+    if(customKey >= '0' && customKey <= '9')
+    {
+      second = second * 10 + (customKey - '0');
+      lcd.setCursor(0,2);
+      lcd.print(second);
+    }
+
+    if(customKey == '=') break;  //return second;
+  }
+ return second; 
+}
